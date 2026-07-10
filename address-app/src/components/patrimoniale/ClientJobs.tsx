f664b85401b0242dcdb7e3b7a2a40d8c335b49db
Patryk Srzedziński <Patryk.Srzedzinski@kruksa.pl>
2026-07-10 13:36:10 +0200

    styling

--- address-app/src/components/patrimoniale/ClientJobs.tsx
+++ address-app/src/components/patrimoniale/ClientJobs.tsx
@@ -24,6 +24,7 @@ export const ClientJobs = ({clientId} : ClientJobsProps) => {
     const [jobs, setJobs] = useState<Job[]>([]);
     const [jobsVersion, setJobsVersion] = useState<number>(0);
     const [newJob, setNewJob] = useState<Job>(createInitialJob);
+    const [isFormVisible, setIsFormVisible] = useState(false);
 
     const handleFieldChange = (field: keyof Job, value: string | boolean | number | null) => {
         setNewJob((prevJob) => ({
@@ -46,92 +47,114 @@ export const ClientJobs = ({clientId} : ClientJobsProps) => {
             setJobs(prev => [...prev, ...response!.clientJobs]);
             setJobsVersion(response.version);
             setNewJob(createInitialJob());
+            setIsFormVisible(false);
         }
     };
 
     return <div className="client-jobs">
-        <h4>New job</h4>
-        <form onSubmit={handleCreateJob}>
-            <div>
-                <label htmlFor="employment-status">Employment status</label>
-                <select
-                    id="employment-status"
-                    value={newJob.clientEmploymentStatus ?? ""}
-                    onChange={(event: ChangeEvent<HTMLSelectElement>) => handleFieldChange("clientEmploymentStatus", event.target.value === "" ? null : Number(event.target.value) as EmploymentStatus)}
-                >
-                    <option value="">Wybierz</option>
-                    {Object.entries(EmploymentStatus)
-                        .filter(([, value]) => typeof value === "number")
-                        .map(([label, value]) => (
-                            <option key={label} value={value}>
-                                {label}
-                            </option>
-                        ))}
-                </select>
-            </div>
-            <div>
-                <label htmlFor="profession">Profession</label>
-                <input
-                    id="profession"
-                    value={newJob.clientProfession ?? ""}
-                    onChange={(event: ChangeEvent<HTMLInputElement>) => handleFieldChange("clientProfession", event.target.value)}
-                />
-            </div>
-            <div>
-                <label htmlFor="confirmed-by-employer">Confirmed by employer</label>
-                <input
-                    id="confirmed-by-employer"
-                    type="checkbox"
-                    checked={newJob.confirmedByEmployer ?? false}
-                    onChange={(event: ChangeEvent<HTMLInputElement>) => handleFieldChange("confirmedByEmployer", event.target.checked)}
-                />
-            </div>
-            <div>
-                <label htmlFor="contract-type">Contract type</label>
-                <select
-                    id="contract-type"
-                    value={newJob.contractType ?? ""}
-                    onChange={(event: ChangeEvent<HTMLSelectElement>) => handleFieldChange("contractType", event.target.value === "" ? null : Number(event.target.value) as ContractType)}
-                >
-                    <option value="">Wybierz</option>
-                    {Object.entries(ContractType)
-                        .filter(([, value]) => typeof value === "number")
-                        .map(([label, value]) => (
-                            <option key={label} value={value}>
-                                {label}
-                            </option>
-                        ))}
-                </select>
-            </div>
-            <div>
-                <label htmlFor="start-date">Start date</label>
-                <input
-                    id="start-date"
-                    type="date"
-                    value={newJob.startDate ?? ""}
-                    onChange={(event: ChangeEvent<HTMLInputElement>) => handleFieldChange("startDate", event.target.value)}
-                />
-            </div>
-            <div>
-                <label htmlFor="end-date">End date</label>
-                <input
-                    id="end-date"
-                    type="date"
-                    value={newJob.endDate ?? ""}
-                    onChange={(event: ChangeEvent<HTMLInputElement>) => handleFieldChange("endDate", event.target.value)}
-                />
-            </div>
-            <div>
-                <label htmlFor="check-date">Check date</label>
-                <input
-                    id="check-date"
-                    type="date"
-                    value={newJob.checkDate ?? ""}
-                    onChange={(event: ChangeEvent<HTMLInputElement>) => handleFieldChange("checkDate", event.target.value)}
-                />
-            </div>
-            <button type="submit">Add job</button>
-        </form>
+        <button type="button" onClick={() => setIsFormVisible(prev => !prev)}>
+            {isFormVisible ? "Cancel" : "Add new job"}
+        </button>
+        {isFormVisible &&
+            <form onSubmit={handleCreateJob} className="client-jobs-form-controls">
+                <table className="client-jobs-form-table">
+                    <tbody>
+                        <tr>
+                            <td className="employment-status-col">
+                                <label htmlFor="employment-status">Employment status</label>
+                                <select
+                                    className="textbox"
+                                    id="employment-status"
+                                    value={newJob.clientEmploymentStatus ?? ""}
+                                    onChange={(event: ChangeEvent<HTMLSelectElement>) => handleFieldChange("clientEmploymentStatus", event.target.value === "" ? null : Number(event.target.value) as EmploymentStatus)}
+                                >
+                                    <option value="">Select</option>
+                                    {Object.entries(EmploymentStatus)
+                                        .filter(([, value]) => typeof value === "number")
+                                        .map(([label, value]) => (
+                                            <option key={label} value={value}>
+                                                {label}
+                                            </option>
+                                        ))}
+                                </select>
+                            </td>
+                            <td className="profession-col">
+                                <label htmlFor="profession">Profession</label>
+                                <input
+                                    className="textbox"
+                                    id="profession"
+                                    value={newJob.clientProfession ?? ""}
+                                    onChange={(event: ChangeEvent<HTMLInputElement>) => handleFieldChange("clientProfession", event.target.value)}
+                                />
+                            </td>
+                            <td className="confirmed-by-employer-col">
+                                <label htmlFor="confirmed-by-employer">Confirmed by employer</label>
+                                <input
+                                    className="checkbox-input"
+                                    id="confirmed-by-employer"
+                                    type="checkbox"
+                                    checked={newJob.confirmedByEmployer ?? false}
+                                    onChange={(event: ChangeEvent<HTMLInputElement>) => handleFieldChange("confirmedByEmployer", event.target.checked)}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="contract-type-col">
+                                <label htmlFor="contract-type">Contract type</label>
+                                <select
+                                    className="textbox"
+                                    id="contract-type"
+                                    value={newJob.contractType ?? ""}
+                                    onChange={(event: ChangeEvent<HTMLSelectElement>) => handleFieldChange("contractType", event.target.value === "" ? null : Number(event.target.value) as ContractType)}
+                                >
+                                    <option value="">Select</option>
+                                    {Object.entries(ContractType)
+                                        .filter(([, value]) => typeof value === "number")
+                                        .map(([label, value]) => (
+                                            <option key={label} value={value}>
+                                                {label}
+                                            </option>
+                                        ))}
+                                </select>
+                            </td>
+                            <td className="start-date-col">
+                                <label htmlFor="start-date">Start date</label>
+                                <input
+                                    className="textbox"
+                                    id="start-date"
+                                    type="date"
+                                    value={newJob.startDate ?? ""}
+                                    onChange={(event: ChangeEvent<HTMLInputElement>) => handleFieldChange("startDate", event.target.value)}
+                                />
+                            </td>
+                            <td className="end-date-col">
+                                <label htmlFor="end-date">End date</label>
+                                <input
+                                    className="textbox"
+                                    id="end-date"
+                                    type="date"
+                                    value={newJob.endDate ?? ""}
+                                    onChange={(event: ChangeEvent<HTMLInputElement>) => handleFieldChange("endDate", event.target.value)}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td colSpan={3} className="check-date-col">
+                                <label htmlFor="check-date">Check date</label>
+                                <input
+                                    className="textbox"
+                                    id="check-date"
+                                    type="date"
+                                    value={newJob.checkDate ?? ""}
+                                    onChange={(event: ChangeEvent<HTMLInputElement>) => handleFieldChange("checkDate", event.target.value)}
+                                />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <button type="submit" className="submit-new-address-button">Add job</button>
+            </form>
+        }
 
         <h4>Jobs list ({jobs.length})</h4>
         {jobs.length === 0 && <p>No jobs.</p>}
